48c66264777b5abb19a135da22601c1e92cccc51
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2022-08-10 14:12:28 +0900

    setup修正

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ from setuptools import find_packages
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
-print(find_packages("src"))
+print("find_packages()",find_packages("src"))
 
 setup(
     name="mask_math",
-    version="0.1.3",
+    version="0.1.4",
     description="私がよく使いまわす数学・アルゴリズムコードを集めたもの",
     author="Mask_coins",
     url="https://github.com/Mask-coins/mask_math",
